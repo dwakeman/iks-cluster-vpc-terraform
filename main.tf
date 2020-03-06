3fcab181eda70e10fd7ab2651ef8d2c7db99db1c
Dave Wakeman <dwakeman@us.ibm.com>
2020-03-06 09:08:18 -0600

    fixed references to data sources

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "ibm_is_subnet" "subnet_zone3" {
 
 resource "ibm_container_vpc_cluster" "cluster" {
   name              = "${var.cluster_name}"
-  vpc_id            = "${ibm_is_vpc.vpc1.id}"
+  vpc_id            = "${data.ibm_is_vpc.vpc1.id}"
   flavor            = "${var.flavor}"
   worker_count      = "${var.worker_count}"
   resource_group_id = "${data.ibm_resource_group.resource_group.id}"
@@ -28,15 +28,15 @@ resource "ibm_container_vpc_cluster" "cluster" {
 
   zones = [
     {
-        subnet_id = "${ibm_is_subnet.subnet_zone1.id}"
+        subnet_id = "${data.ibm_is_subnet.subnet_zone1.id}"
         name      = "${var.region}-1"
     },
     {
-        subnet_id = "${ibm_is_subnet.subnet_zone2.id}"
+        subnet_id = "${data.ibm_is_subnet.subnet_zone2.id}"
         name      = "${var.region}-2"
     },
     {
-        subnet_id = "${ibm_is_subnet.subnet_zone3.id}"
+        subnet_id = "${data.ibm_is_subnet.subnet_zone3.id}"
         name      = "${var.region}-3"
     }
 
